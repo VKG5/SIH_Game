895e80453b1e08449ab81ee547f6a5d74a98dd8b
Lumi_V <bawagupta69@gmail.com>
2020-01-26 23:40:30 +0530

    Initial Game

---
+++ Assets/Scripts/AdventureScript.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureScript : MonoBehaviour
+{
+    [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+
+    int reward_point = 0;
+    State state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = startingState;
+        // The string inside the textComponent field
+        textComponent.text = state.GetStoryState();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+        var nextStates = state.GetNextStates();
+        
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            state = nextStates[0];
+        }
+
+        for(int index=0; index<nextStates.Length; index++)
+        {
+            if(Input.GetKeyDown(KeyCode.Alpha1 + index))
+            {
+                rewards(index);
+                
+                state = nextStates[index];
+            }
+        }
+        
+        textComponent.text = state.GetStoryState();
+    }
+
+    private void rewards(int index)
+    {
+        if((index+1)==1)
+            reward_point += 0;
+
+        else if((index+1)==2)
+            reward_point += 5;
+
+        else
+            reward_point += 10;
+
+        Debug.Log("Reward: "+(reward_point));
+    }
+}
---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] float waitInSeconds = 1.5f;
+    int currSceneIndex;
+    // Start is called before the first frame update
+    void Start()
+    {   
+        currSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if(currSceneIndex==0)
+        {
+            StartCoroutine(WaitForTime());
+        }
+    }
+
+    IEnumerator WaitForTime()
+    {
+        yield return new WaitForSeconds(waitInSeconds);
+        LoadNextScene();
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currSceneIndex + 1);
+    }
+}
